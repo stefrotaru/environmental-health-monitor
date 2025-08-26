a01c661e111103bc8c5cb7e1453f619a18a44c9e
Stefan Rotarue <stefanrotaru@hotmail.com>
2025-08-26 23:29:11 +0300

    add heroicons, primevue, chartjs dependencies; update nuxt config

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <MainPage />
   </div>
 </template>
+
+<script setup>
+import MainPage from '../pages/MainPage.vue'
+
+// CSS imports
+import '../assets/css/_index.css'
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  // compatibilityDate: '2025-08-24',
+  runtimeConfig: {
+    openaqApiKey: process.env.OPENAQ_API_KEY
+  },
+  modules: [
+      '@primevue/nuxt-module'
+  ],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+        theme: {
+            preset: Aura,
+            options: {
+              prefix: 'p-',
+              // Scope all PrimeVue styles to this class
+              darkModeSelector: '.dark-theme',
+            }
+        }
+    }
+  },
+  devtools: { enabled: true },
+  // css: [
+  //   "~/assets/css/reset.css",
+  //   "~/assets/css/variables.css",
+  //   "~/assets/css/base.css",
+  //   "~/assets/css/components/icon.css",
+  //   "~/assets/css/components/health-alert.css",
+  //   "~/assets/css/components/weather-card.css",
+  // ],
+  // postcss: {
+  //   plugins: {
+  //     autoprefixer: {},
+  //   },
+  // },
+});
